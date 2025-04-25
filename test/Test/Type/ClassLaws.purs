a6d6e9df91a88487b9dfd2f805e7e4056ed46e63
UnrelatedString <unrelatedstring@gmail.com>
2025-04-25 00:17:12 -0400

    take 2

---
+++ test/Test/Type/ClassLaws.purs
@@ -0,0 +1,19 @@
+module Test.Type.ClassLaws
+  ( functorLaws
+  , applyLaws
+  , applicativeLaws
+  , bindLaws
+  , monadLaws
+  ) where
+
+import Prelude
+
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Console as Console
+import Test.Assert as Assert
+
+type FromArray f = forall a. Array a -> Maybe (f a)
+
+type LawsTest :: forall k. (k -> Constraint) -> Type
+type LawsTest c = forall f. c f => 
