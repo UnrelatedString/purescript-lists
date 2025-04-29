46d027340cc9948356f2457f5b209a610ee2ee5d
UnrelatedString <unrelatedstring@gmail.com>
2025-04-29 17:48:37 -0400

    ...aaaaaaaaaand of course local deps can't read bower. whatever at least this is *getting* there and a vastly better idea than the other stuff I tried. afraid of actually putting a spago.yaml in the top level since that could mess with publishing though... copying the dependencies in seems bad for maintainability but for something this low level with so few dependencies I guess it should be Fine

---
+++ quickcheck/.spago/BuildInfo.purs
@@ -0,0 +1,15 @@
+-- @inline export packages always
+-- @inline export pursVersion always
+-- @inline export spagoVersion always
+module Spago.Generated.BuildInfo where
+
+packages :: { "lists-spec-tests" :: String }
+packages =
+  { "lists-spec-tests": "0.0.0"
+  }
+
+pursVersion :: String
+pursVersion = "0.15.15"
+
+spagoVersion :: String
+spagoVersion = "0.93.43"
---
+++ quickcheck/test/Test/Main.purs
@@ -0,0 +1,42 @@
+module Test.Main where
+
+import Prelude
+
+import Effect (Effect)
+import Test.QuickCheck ((===), class Testable, class Arbitrary, class Coarbitrary)
+import Test.Spec (Spec, describe, it)
+import Test.Spec.QuickCheck (quickCheck)
+import Test.Spec.Runner.Node (runSpecAndExitProcess)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Type.Proxy (Proxy(..))
+
+import Data.List.Types (List, NonEmptyList)
+import Data.List.ZipList (ZipList)
+import Data.List.Lazy.Types as LZ
+
+-- Proxy a is Discard ;)
+type For f =
+  Eq (f Value) =>
+  Show (f Value) =>
+  Arbitrary (f Value) =>
+  Arbitrary (f (Value -> Value)) =>
+  Coarbitrary (f Value) =>
+  Spec (Proxy f)
+
+type Value = Int
+
+proxied :: forall m f. m Unit -> m (Proxy f)
+proxied = (_ $> Proxy)
+
+main :: Effect Unit
+main = runSpecAndExitProcess [consoleReporter] do
+  describe 
+  functorLaws :: For List
+
+functorLaws :: forall f. For f
+functorLaws = proxied $ describe "Functor laws" do
+  it "Identity: map identity = identity" do
+    quickCheck \(l :: f Value) -> map identity l === l
+  it "Composition: map (f <<< g) = map f <<< map g" do
+    quickCheck \(l :: f Value) -> map (f <<< g) l === map f (map g l)
+
