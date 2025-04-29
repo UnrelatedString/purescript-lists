2347435596bc1f2518e663dd3fb8a4c72c10357b
UnrelatedString <unrelatedstring@gmail.com>
2025-04-29 16:52:18 -0400

    i do not remember or care what i had left here lmao

--- test/Test/Type/ClassLaws.purs
+++ test/Test/Type/ClassLaws.purs
@@ -16,4 +16,5 @@ import Test.Assert as Assert
 type FromArray f = forall a. Array a -> Maybe (f a)
 
 type LawsTest :: forall k. (k -> Constraint) -> Type
-type LawsTest c = forall f. c f => 
+type LawsTest c = forall f.
+  c f => 
